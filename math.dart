c90640fb2e4a21c5a4c335b983aaae0f217ef640
Md Abdullah Al Siddik <119064797+mdabdullahalsiddik@users.noreply.github.com>
2023-04-26 01:32:22 -0700

    Add files via upload

---
+++ math.dart
@@ -0,0 +1,41 @@
+void main(List<String> args) {
+  a();
+  b();
+  c();
+  e();
+}
+
+void a() {
+  var a, b;
+  a = 10;
+  b = 15;
+  print(a + b);
+}
+
+void b() {
+  var a, b;
+  a = 10;
+  b = 15;
+  print(a - b);
+}
+
+void c() {
+  var a, b;
+  a = 10;
+  b = 15;
+  print(a * b);
+}
+
+void d() {
+  var a, b;
+  a = 10;
+  b = 15;
+  print(a / b);
+}
+
+void e() {
+  var a, b;
+  a = 10;
+  b = 15;
+  print(a % b);
+}
